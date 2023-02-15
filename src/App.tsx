ad2bc8eda274046d48d89f5cd7757890b13debcc
Guilherme <guilhermepolicarpio@gmail.com>
2023-02-15 13:34:44 -0300

    feat: initial styling ajustments

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
+import styles from './App.module.css'
+
 function App() {
   return (
     <div>
       <Header />
-      <>Conteudo...</>
+      <main className={styles.main}>
+
+        <h1>Conteudo...</h1>
+      </main>
+
       <Footer />
 
     </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-
+import styles from "./Footer.module.css"
 const Footer = () => {
     return (
-        <div>
-            <footer>
+            <footer className={styles.footer}>
                 <p>
                     <span>
                         React Todo
                     </span>
                 </p>
             </footer>
-        </div>
     );
 }
 
