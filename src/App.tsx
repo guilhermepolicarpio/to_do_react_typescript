f45f00dea390983d33af45b92951ad8864e330f2
Guilherme <guilhermepolicarpio@gmail.com>
2023-02-15 11:29:24 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <>Conteudo...</>
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <p>
+                    <span>
+                        React Todo
+                    </span>
+                </p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
